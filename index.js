1002716f0854fc4c57d7b6079cee99c477342307
Afra Kucukaydin <afrakucukaydin@gmail.com>
2023-03-20 23:57:26 +0300

    Halfly completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards (name, event) {
+ let message = [];
+ for (let = i; i < name.length; i++) {
+    message.push("Thank you, ${name[i]}, for the wonderful ${event} gift!");
+ }
+ return message;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+    }
+}
+
+
